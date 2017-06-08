9867c65b3bd71758439dc5ac36bf13b1d6d5e518
Aniket sharma <aniket965.as@gmail.com>
2017-06-08 19:30:15 +0530

    Initial commit

---
+++ app/src/main/java/com/scibots/anike/mre_fbgroup/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.scibots.anike.mre_fbgroup;
+
+import android.os.Build
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // for making status bar completly transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            val w = window // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
+
+
+
+    }
+}
